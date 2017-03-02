c8c8773be2242f00c40a46fae0b77ce653b6390e
marichards <marichards88@gmail.com>
2017-03-02 15:47:29 -0800

    Changed seed and it works better

--- predictNextWord.R
+++ predictNextWord.R
@@ -3,7 +3,8 @@ library(tokenizers)
 library(stringr)
 suppressMessages(library(tm))
 library(hash)
-suppressMessages(library(dtplyr))
+suppressMessages(library(dplyr))
+suppressMessages(library(data.table))
 
 constructNgramTables <- function(){
 
@@ -14,8 +15,8 @@ constructNgramTables <- function(){
   en.twitter <- readLines(con = "../final/en_US/en_US.twitter.txt", warn=FALSE, skipNul = TRUE)
   invisible(gc())
   
-  # Take 10% of each
-  set.seed(140185)
+  # Take 10% of each (or 20% if stopwords are removed?)
+  set.seed(10185) #140185 seems to not work well
   en.blog <- sample(en.blog,round(length(en.blog)/10))
   en.news <- sample(en.blog,round(length(en.blog)/10))
   en.twitter <- sample(en.blog,round(length(en.blog)/10))
@@ -30,10 +31,12 @@ constructNgramTables <- function(){
   ############## Dataset is now clean ##################  en.data <- tolower(en.data)
   # Try removing stop words
   en.data <- tolower(en.data)
-#  en.data <- removeWords(en.data, stopwords())
+
   en.data <- removePunctuation(en.data)
   en.data <- removeNumbers(en.data)
   en.data <- stripWhitespace(en.data)
+#  en.data <- removeWords(en.data, stopwords())
+  en.data <- trimws(en.data)
   en.data <- iconv(en.data,"UTF-8","ASCII",sub="")
   
   # Remove bad words
@@ -122,38 +125,68 @@ constructNgramTables <- function(){
               singlet.inverted)) ## THis function currently takes 4 minutes...not bad! 
   #List is 153 Mb total, but 76.5 if you drop a quin; they're basically the same size
   ## With all tables, now this takes 15-16 min and produces 220 Mb...not bad!
+  # Using 20% and no stop words, takes 24 min and 254.3 Mb
+  # Using 20% and all the words, takes 46 min and 515 Mb
 }
 
 predictNextWord <- function(phrase,table.list){
   # Take in the input and grab the last n grams for 4, 3, 2, 1
   # Remove stop words
-#  phrase <- removeWords(phrase,stopwords())
+  phrase <- tolower(phrase)
   phrase <- removePunctuation(phrase)
+  phrase <- removeNumbers(phrase)
+#  phrase <- removeWords(phrase,stopwords())
+  phrase <- trimws(phrase)
+
+  
   
   # Grab grams as their indices in the hash
-  gram4 <- table.list[[5]][[tolower(word(phrase, -1))]]
-  gram3 <- table.list[[5]][[tolower(word(phrase, -2))]]
-  gram2 <- table.list[[5]][[tolower(word(phrase, -3))]]
-  gram1 <- table.list[[5]][[tolower(word(phrase, -4))]]
+  gram4 <- table.list[[5]][[word(phrase, -1)]]
+  gram3 <- table.list[[5]][[word(phrase, -2)]]
+  gram2 <- table.list[[5]][[word(phrase, -3)]]
+  gram1 <- table.list[[5]][[word(phrase, -4)]]
   
   # Filter answers that share the last gram and so forth using dplyr
   last4 <- filter(table.list[[1]],
                   word.1 == gram1 & word.2 == gram2 & word.3 == gram3 & word.4 == gram4)
   # Going forward, don't let the last gram be the same as previous
   last3 <- filter(table.list[[2]], 
-                  word.1 == gram2 & word.2 == gram3 & word.3 == gram4 & !(word.4 %in% last4$word.5))
+                  word.1 == gram2 & word.2 == gram3 & word.3 == gram4)# & !(word.4 %in% last4$word.5))
   last2 <- filter(table.list[[3]], 
-                  word.1 == gram3 & word.2 == gram4 & !(word.3 %in% last4$word.5) & !(word.3 %in% last3$word.4))
+                  word.1 == gram3 & word.2 == gram4)# & !(word.3 %in% last4$word.5) & !(word.3 %in% last3$word.4))
   
   last1 <- filter(table.list[[4]], 
-                  word.1 == gram4 & !(word.2 %in% last4$word.5) & !(word.2 %in% last3$word.4) & !(word.2 %in% last2$word.3))
+                  word.1 == gram4 )#& !(word.2 %in% last4$word.5) & !(word.2 %in% last3$word.4) & !(word.2 %in% last2$word.3))
 
   # Now we have unique things
-  ## Grab the table of each one, but eliminate things we've already seen
+  
+
+ 
+  ## Grab the table of each one, summing up frequencies
   tbl.5gram <- sort(table(last4$word.5),decreasing=TRUE)
   tbl.4gram <- sort(table(last3$word.4),decreasing=TRUE)
   tbl.3gram <- sort(table(last2$word.3),decreasing=TRUE)
   tbl.2gram <- sort(table(last1$word.2),decreasing=TRUE)
   
+  # Filter out stopwords? 
+  
+  # Return things if they're not null
+  if(length(tbl.5gram) !=0)
+  {pred5 <- table.list[[6]][[names(tbl.5gram)[[1]]]]}
+  else{pred5 <- NA}
+  if(length(tbl.4gram) !=0)
+  {pred4 <- table.list[[6]][[names(tbl.4gram)[[1]]]]}
+  else{pred4 <- NA}
+  if(length(tbl.3gram) !=0)
+  {pred3 <- table.list[[6]][[names(tbl.3gram)[[1]]]]}
+  else{pred3 <- NA}
+  if(length(tbl.2gram) !=0)
+  {pred2 <- table.list[[6]][[names(tbl.2gram)[[1]]]]}
+  else{pred2 <- NA}
+  
+  my.prediction <- data.frame(pred5 = pred5,
+                              pred4 = pred4,
+                              pred3 = pred3,
+                              pred2 = pred2)
   return(my.prediction)
 }
